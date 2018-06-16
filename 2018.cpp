eadf32acf957056fc83d6dd741e85f704b450a03
Adam Blakey <adam@blakey.family>
2018-06-16 17:05:41 +0100

    Got it compile!

--- 2018.cpp
+++ 2018.cpp
@@ -1,10 +1,28 @@
+#include <ctime>
 #include <iostream>
+#include <string>
 
-int 
+int numberOfFathersDays(string childrenNames[], int childrenDOBs[]);
 
+int numberOfFathersDays(string childrenNames[], int childrenDOBs[])
+{
+	int time = std::time(0);
+
+	return time;
+}
 
 int main()
 {
-	std::cout << "You have celebrated " << numberOfFathersDays() << " as a father." << endl;
+	time_t now = std::time(0);
+
+	string names[] = {"Adam", "Ben", "Lauren"};
+	int ages[] = {2345, 2345, 1234};
+
+	int n = numberOfFathersDays(names, ages);
+
+	// {"Adam", "Ben", "Lauren"}, {880243200, 993686400, 1122854400}
+	
+
+	std::cout << "You have celebrated " << n << " years as a father." << endl;
 	return 0;
 }
