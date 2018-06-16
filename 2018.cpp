776ec31571fcbdbfa2ba3ff89d49077e97c490c6
Adam Blakey <adam@blakey.family>
2018-06-16 22:33:58 +0100

    Things have broken... But progress has been made!

--- 2018.cpp
+++ 2018.cpp
@@ -10,6 +10,8 @@
 	int numberOfFathersDays(string childrenNames[], int childrenDOBs[]);
 	// Returns the timestamp at midnight on the third Sunday of June in the given year.
 	time_t thirdSundayOfJune(int year);
+	// Returns the timestamp on 1st January at 00:00:00 for that year.
+	int yearToTimestamp(int year);
 
 // FUNCTION DEFINITIONS
 	bool leapYear(int year)
@@ -46,13 +48,29 @@
 
 		// Outputs the number of Fathers' Days celebrated for those children.
 		std::cout << "You have celebrated " << numberOfFathersDays(names, DOBs) << " Fathers' Days as a father." << endl;
+
+		thirdSundayOfJune(2018);
+
+		int test[] = {3, 5, 7, 9};
+
+		
+	}
+
+	int minimum(int array[])
+	{
+		for (int i=0; i<=sizeof(array); ++i)
+		{
+
+		}
 	}
 
 	int numberOfFathersDays(string childrenNames[], int childrenDOBs[])
 	{
 		time_t now = std::time(0);
 
-		return 1;
+		
+
+		return now;
 	}
 
 	time_t thirdSundayOfJune(int year)
@@ -61,7 +79,41 @@
 		const int June = 5; // Month number (0,11).
 		const int day = 6; // Day number (0,6).
 		const int daysBeforeJune = 31 + 28 + 31 + 30 + 31; // Days before June on a non-leap year.
-		const int daysBeforeJuneLeap = daysBeforeJune + 1; // Days before June on a leap year.
 
+		time_t now = std::time(0);
+		struct tm* nowInfo = localtime(&now);
 
+		int thisYear = nowInfo->tm_year + 1900; // tm_year returns years from 1900.
+		int thisYearTimestamp = yearToTimestamp(thisYear);
+		int thisFathersDay = thisYearTimestamp + (daysBeforeJune + leapYear(thisYear))*60*60*24; //WRONG
+
+		int lastYear = thisYear - 1;
+		int lastYearTimestamp = yearToTimestamp(lastYear); 
+		int lastFathersDay = lastYearTimestamp + (daysBeforeJune + leapYear(lastYear))*60*60*24; //WRONG
+
+		int mostRecentFathersDay = 0;
+
+		if (now < thisFathersDay)
+		{
+			mostRecentFathersDay = lastFathersDay;
+		}
+		else
+		{
+			mostRecentFathersDay = thisFathersDay;
+		}
+
+		return mostRecentFathersDay;
+	}
+
+	int yearToTimestamp(int year)
+	{
+		int timestamp = 0;
+
+		for (int i = 1970; i <= year; i++)
+		{
+			timestamp += (365 + leapYear(i)) * 60*60*24; // (days) * seconds*minutes*hours.
+
+		}
+
+		return timestamp;
 	}
